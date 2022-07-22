8877057d59c37fbf1a1f3f7a923c3727bc3079d4
Kev <haokliao@gmail.com>
2022-07-21 21:29:04 -0400

    feat(components): Starting refactoring for components

--- client/src/App.js
+++ client/src/App.js
@@ -1,52 +1,10 @@
-import React, {useEffect, useState} from 'react';
+import React  from 'react';
 import { Helmet } from 'react-helmet';
 import './App.css';
 import HKL_resume from './imgs/HKL_Resume_3_14.pdf'
 import {PROJECTS} from './data.js'
-
-function RecentlyPlayed(props) {
-  return (
-    <>
-        <div className='listening'>
-      <div className='big-txt'>
-        <a href='https://open.spotify.com/user/1242975125'>I’m</a> currently listening to..</div>       
-      <div className='listening-container'>
-        <div className='listening-album'>
-          <img src = {props.albumart} alt ='album_cover'/>
-        </div>
-        <div className='listening-text'>
-          <a href={props.trackurl} target='_blank' rel='noreferrer'>
-            {props.tracksname}
-          </a>
-          <span> by</span> {props.artistsname}
-        </div>
-      </div> 
-    </div>
-    </>
-  )
-
-}
-
-function Projects(props){
-  return(
-    <div className= {'projects blue-border ' + props.items.grid}>
-        <div className='proj-header'>
-          <p>Projects - Web Development</p>        
-        </div>
-
-        <div>
-          <p className='big-txt'>{props.items.title}</p>
-          <p className='small-txt'>{props.items.tech}</p>
-          <p className='small-txt'>{props.items.description}
-          </p>
-          <a href={props.items.link} >
-          <img src={require('../src/' + props.items.img)} id='smaller-pics' alt='project thumbnail'/>
-          </a>
-        </div>
-    </div>
-
-  )
-}
+import RecentlyPlayed from './components/RecentlyPlayed';
+import Projects from './components/Projects';
 
 
 function App() {
@@ -58,52 +16,7 @@ function App() {
         />
       })
     
-
-
-  const [tracks,getTracks] = useState([
-  {
-    track: {
-      album: {        
-        images: [
-          {
-            empty: ''
-          },
-          {
-            empty1: ''
-          },
-          {
-            empty2: '',
-            url: 'https://i.scdn.co/image/ab67616d000048518dbdac761ce552c2ac7d69e1'
-          }
-        ],
-        artists: 
-          [
-            {
-              external_urls: {
-                spotify: 'https://open.spotify.com/artist/0knGpCTbmG4ctl1wzYRZs4'
-              },
-              name: 'Dijon'
-            },
-          ],
-      },
-      name: 'Rodeo Clown',
-      external_urls: {
-        spotify: 'https://open.spotify.com/track/1km9AsmFeKZpeR4VvPYsJh'
-      }
-    }
-  }
-  ]);
-
-  useEffect(() => {
-    document.title = 'Hao Kai Liao'
-    fetch('/recently_played')
-    .then((resp) => {
-      resp.json()
-      .then(tracks =>getTracks(tracks.payload.items))
-      })
-      .catch((err) => console.error(err))
-  }, []);
-
+  
   return(
 <>
   <div className="grid-container">
@@ -137,15 +50,15 @@ function App() {
       </table>
 
       <div className='icons'>
-        <a href={HKL_resume} target='_blank' rel='noreferrer'
+        <a href={HKL_resume} target='_blank' rel='noopener noreferrer'
           className='text-decoration-none' title='Resume!'>
           <img src= {require('./imgs/resume_icon.png')} style={{cursor:'pointer'}} className='project-imgs' alt='Resume PDF Icon'/>
         </a>
-        <a href='https://www.linkedin.com/in/haokliao/' target='_blank' rel='noreferrer'
+        <a href='https://www.linkedin.com/in/haokliao/' target='_blank' rel='noopener noreferrer'
           className ='text-decoration-none text-linkedin' title='Linkedin'>
           <img src= {require('./imgs/li_icon.png')} className='project-imgs' alt='find me on Linkedin, @haokliao'/>
         </a>
-        <a href='https://www.github.com/haokliao'  target='_blank' rel='noreferrer'
+        <a href='https://www.github.com/haokliao'  target='_blank' rel='noopener noreferrer'
           className='text-decoration-nonetext-github text-decoration-none' title='Github' >
           <img src= {require('./imgs/github_icon.png')} className='project-imgs' alt='find me on github, @haokliao'/>
         </a>
@@ -161,13 +74,10 @@ function App() {
       </div>
     </div>
 
-    <RecentlyPlayed
-      artistsname={tracks[0].track.album.artists[0].name}
-      tracksname={tracks[0].track.name}
-      trackurl={tracks[0].track.external_urls.spotify}
-      albumart={tracks[0].track.album.images[2].url}/>
 
+    <RecentlyPlayed/>
     {projectElements}
+
 
   </div> 
 </>
---
+++ client/src/components/RecentlyPlayed.jsx
@@ -0,0 +1,77 @@
+import React, {useEffect, useState} from 'react';
+import '../App.css'
+
+
+export default function RecentlyPlayed(props) {
+
+  const [tracks,getTracks] = useState([
+  {
+    track: {
+      album: {        
+        images: [
+          {
+            empty: ''
+          },
+          {
+            empty1: ''
+          },
+          {
+            empty2: '',
+            url: 'https://i.scdn.co/image/ab67616d000048518dbdac761ce552c2ac7d69e1'
+          }
+        ],
+        artists: 
+          [
+            {
+              external_urls: {
+                spotify: 'https://open.spotify.com/artist/0knGpCTbmG4ctl1wzYRZs4'
+              },
+              name: 'Dijon'
+            },
+          ],
+      },
+      name: 'Rodeo Clown',
+      external_urls: {
+        spotify: 'https://open.spotify.com/track/1km9AsmFeKZpeR4VvPYsJh'
+      }
+    }
+  }
+  ]);
+
+  useEffect(() => {
+    document.title = 'Hao Kai Liao'
+    fetch('/recently_played')
+    .then((resp) => {
+      resp.json()
+      .then(tracks => getTracks(tracks.payload.items))
+      })
+      .catch((err) => console.error(err))
+  }, []);
+
+
+
+
+
+  return (
+    <>
+    <div className='listening'>
+        <div className='big-txt'>
+          <a href='https://open.spotify.com/user/1242975125' target='_blank' rel='noopener noreferrer'>I’m</a> currently listening to..
+        </div>       
+        <div className='listening-container'>
+          <div className='listening-album'>
+            <img src = {tracks[0].track.album.images[2].url} alt ='album_cover'/>
+          </div>
+          <div className='listening-text'>
+            <a href={tracks[0].track.external_urls.spotify} target='_blank' rel='noopener noreferrer'>
+              {tracks[0].track.name}
+            </a>
+            <span> by</span> {tracks[0].track.album.artists[0].name}
+          </div>
+        </div> 
+      </div>     
+    </>
+  )
+
+}
+
